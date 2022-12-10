83062753cdf476bdeff9f64adb2a5cf192ce9748
GeekGarage <rasmus@geekgarage.dk>
2022-12-10 10:32:33 +0100

    Fixed Idle Cam For Vehicles (PR #6)
    * Fix Vehicle Idle Cam
    * Added Chat Suggestions

--- client.lua
+++ client.lua
@@ -18,10 +18,19 @@ RegisterCommand('idlecamon', function() -- help2 31, 167, 9
   SetResourceKvp("idleCam", "on")
 end)
 
+
+TriggerEvent("chat:addSuggestion", "/idlecamon", "Re-enables the idle cam")
+TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
+
+local IdleCamState = GetResourceKvpString("idleCam") == "off"
+
 Citizen.CreateThread(function()
-  TriggerEvent("chat:addSuggestion", "/idlecamon", "Re-enables the idle cam")
-  TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
-  
-  local idleCamDisabled = GetResourceKvpString("idleCam") == "off" --- Idle Cam is on by default for new players / first time adding to server.
-  DisableIdleCamera(idleCamDisabled)                               --- If you would prefer to have it off by default, change the value to "on"
+	while true do
+    IdleCamState = GetResourceKvpString("idleCam")
+    if IdleCamState == "off" then
+		  InvalidateIdleCam()
+		  InvalidateVehicleIdleCam()
+    end
+    Citizen.Wait(10000) --The idle camera activates after 30 second so we don't need to call this per frame
+	end
 end)
