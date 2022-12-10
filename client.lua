5ae3f6415d677341bdf329e61a7e41fdf5914fff
GeekGarage <info@geekgarage.dk>
2022-12-10 06:54:54 +0100

    Update debug print

--- client.lua
+++ client.lua
@@ -23,14 +23,13 @@ TriggerEvent("chat:addSuggestion", "/idlecamon", "Re-enables the idle cam")
 TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
 
 local IdleCamState = GetResourceKvpString("idleCam") == "off"
-print(IdleCamState)
-print("We Rollin'")
+
 Citizen.CreateThread(function()
 	while true do
     IdleCamState = GetResourceKvpString("idleCam")
     print(IdleCamState)
     if IdleCamState == "off" then
-      print("I'm In!")
+      print("resetting idle timer!")
 		  InvalidateIdleCam()
 		  InvalidateVehicleIdleCam()
     end
