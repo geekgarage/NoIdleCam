4c3055f237af0a6bf1181663f96b441fc9937fa2
GeekGarage <info@geekgarage.dk>
2022-12-10 23:16:12 +0100

    Remove Redundant Code & Fixed Spacing
    * Fix Vehicle Idle Cam
    
    * Fix Idle Cam
    
    * Removed Unnecessary Code
    
    * Removed Redundant Code
    
    * Fix Spacing

--- client.lua
+++ client.lua
@@ -4,7 +4,6 @@ RegisterCommand('idlecamoff', function() -- help2 31, 167, 9
     multiline = true,
     args = {'[COMMANDS]', 'Idle Cam Is Now Off'}
   })
-  DisableIdleCamera(true)
   SetResourceKvp("idleCam", "off")
 end)
 
@@ -14,10 +13,8 @@ RegisterCommand('idlecamon', function() -- help2 31, 167, 9
     multiline = true,
     args = {'[COMMANDS]', 'Idle Cam Is Now On'}
   })
-  DisableIdleCamera(false)
   SetResourceKvp("idleCam", "on")
 end)
-
 
 TriggerEvent("chat:addSuggestion", "/idlecamon", "Re-enables the idle cam")
 TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
@@ -25,12 +22,12 @@ TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
 local IdleCamState = GetResourceKvpString("idleCam") == "off"
 
 Citizen.CreateThread(function()
-	while true do
+  while true do
     IdleCamState = GetResourceKvpString("idleCam")
     if IdleCamState == "off" then
-		  InvalidateIdleCam()
-		  InvalidateVehicleIdleCam()
+      InvalidateIdleCam()
+      InvalidateVehicleIdleCam()
     end
     Citizen.Wait(10000) --The idle camera activates after 30 second so we don't need to call this per frame
-	end
+  end
 end)
